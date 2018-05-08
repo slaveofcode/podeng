d45e24555fd31206b47bc80d75ed94db446e7d7e
Aditya Kresna Permana <zeandcode@gmail.com>
2018-05-08 10:08:31 +0700

    Added normalized function in string

--- blueprint/index.js
+++ blueprint/index.js
@@ -1,62 +1,70 @@
-'use strict';
+'use strict'
 
-const _ = require('lodash');
-const types = require('../types');
+const _ = require('lodash')
+const types = require('../types')
 
 const createHandler = (schema, isArrayType = false) => {
-  const cls = function(schema, { isArray = false }) {
-    this.isArray = isArray;
-    this.schema = schema;
-    this.processValue = processValue;
-    this.serialize = serializeValue;
-    this.deserialize = deserializeValue;
-  };
+  const cls = function (schema, { isArray = false }) {
+    this.isArray = isArray
+    this.schema = schema
+    this.normalizeValue = normalizeValue
+    this.serialize = serializeValue
+    this.deserialize = deserializeValue
+  }
 
-  const processValue = function(values) {
-    console.log(`Value processed with schema: ${this.schema}`);
-    return [false, null, { key1: 'fooo' }];
-  };
+  const normalizeValue = function (valuesToNormalize) {
+    const normalizedResult = {}
+    const errorResults = []
+    _.forEach(this.schema, (typeHandler, key) => {
+      const handler = typeHandler()
+      const [fail, normalizedValue] = handler.parse(valuesToNormalize[key])
+      if (!fail || (fail && !handler.isHideOnFail())) {
+        normalizedResult[key] = normalizedValue
+      }
+    })
+    return [false, null, normalizedResult]
+  }
 
-  const serializeValue = function(values) {
-    console.log(`Serialize value processed with schema: ${this.schema}`);
-    return [false, null, { serializedkey1: 'fooo' }];
-  };
+  const serializeValue = function (values) {
+    console.log(`Serialize value processed with schema: ${this.schema}`)
+    return [false, null, { serializedkey1: 'fooo' }]
+  }
 
-  const deserializeValue = function(values) {
-    console.log(`Deserialize value processed with schema: ${this.schema}`);
-    return [false, null, { deserializedkey1: 'fooo' }];
-  };
+  const deserializeValue = function (values) {
+    console.log(`Deserialize value processed with schema: ${this.schema}`)
+    return [false, null, { deserializedkey1: 'fooo' }]
+  }
 
   const initHandler = () => {
-    const obj = new cls(schema, { isArrayType });
+    const obj = new cls(schema, { isArrayType })
 
     const doProcessValue = values => {
-      console.log('Evaluating input values');
-      console.log('Object process calling: ');
-      return obj.processValue(values);
-    };
+      console.log('Evaluating input values')
+      console.log('Object process calling: ')
+      return obj.normalizeValue(values)
+    }
 
     // default process
-    const handlerFunc = values => doProcessValue(values);
+    const handlerFunc = values => doProcessValue(values)
 
-    handlerFunc.getObject = () => obj;
-    handlerFunc.getClass = () => cls;
+    handlerFunc.getObject = () => obj
+    handlerFunc.getClass = () => cls
 
-    return handlerFunc;
-  };
+    return handlerFunc
+  }
 
-  return initHandler();
-};
+  return initHandler()
+}
 
 const objectComponent = (
   params,
-  options = { giveWarning: false, throwOnError: false },
+  options = { giveWarning: false, throwOnError: false }
 ) => {
-  const handler = createHandler(params);
+  const handler = createHandler(params)
 
   const errorHandler = (
     isWarning,
-    { errorDetails, throwOnError, giveWarning },
+    { errorDetails, throwOnError, giveWarning }
   ) => {
     if (isWarning) {
       // check if giveWarning type is object, probably should display warning on specific key with custom handler
@@ -65,51 +73,62 @@ const objectComponent = (
       // check if throwOnError type is object, probably should throw error on specific key with custom handler
       // throw error here
     }
-  };
+  }
 
-  const component = function(values) {
-    const [err, errDetails, normalizedValues] = handler(values);
-  };
+  const component = function (values) {
+    const [err, errDetails, normalizedValues] = handler(values)
+    if (err) {
+      errorHandler(true, {
+        errorDetails,
+        throwOnError: options.throwOnError,
+        giveWarning: options.giveWarning
+      })
+    }
+
+    return normalizedValues
+  }
 
   component.serialize = values => {
     const [err, errorDetails, serializedValues] = handler
       .getObject()
-      .serialize(values);
+      .serialize(values)
 
-    if (err)
+    if (err) {
       errorHandler(true, {
         errorDetails,
         throwOnError: options.throwOnError,
-        giveWarning: options.giveWarning,
-      });
+        giveWarning: options.giveWarning
+      })
+    }
 
-    return serializedValues;
-  };
+    return serializedValues
+  }
 
   component.deserialize = values => {
     const [
       err,
       errorDetails,
-      deserializedValues,
-    ] = handler.getObject().deserialize(values);
+      deserializedValues
+    ] = handler.getObject().deserialize(values)
 
-    if (err)
+    if (err) {
       errorHandler(true, {
         errorDetails,
         throwOnError: options.throwOnError,
-        giveWarning: options.giveWarning,
-      });
+        giveWarning: options.giveWarning
+      })
+    }
 
-    return deserializedValues;
-  };
+    return deserializedValues
+  }
 
-  return component;
-};
+  return component
+}
 
-const arrayComponent = (params, options) => {};
+const arrayComponent = (params, options) => {}
 
 module.exports = {
   object: objectComponent,
   array: arrayComponent,
-  types,
-};
+  types
+}
--- types/string.js
+++ types/string.js
@@ -1,52 +1,72 @@
-'use strict';
+'use strict'
 
-const { isFunction } = require('./detector');
-const _ = require('lodash');
+const { isFunction, isString } = require('./detector')
+const _ = require('lodash')
 
 const NORMALIZER = {
+  trimmed: value => _.trim(value),
   uppercased: value => _.toUpper(value),
   lowercased: value => _.toLower(value),
   upper_first: value => _.upperFirst(value),
   upper_first_word: value => {
-    let upperCasedFirst = [];
-    _.forEach(value.split(/\s/g), v => upperCasedFirst.push(_.toUpper(v)));
-    return upperCasedFirst.length > 0 ? upperCasedFirst.join(' ') : value;
+    let upperCasedFirst = []
+    _.forEach(value.split(/\s/g), v => upperCasedFirst.push(_.toUpper(v)))
+    return upperCasedFirst.length > 0 ? upperCasedFirst.join(' ') : value
   },
   lower_first: value => _.lowerFirst(value),
   lower_first_word: value => {
-    let lowerCasedFirst = [];
-    _.forEach(value.split(/\s/g), v => lowerCasedFirst.push(_.toLower(v)));
-    return lowerCasedFirst.length > 0 ? lowerCasedFirst.join(' ') : value;
-  },
-};
+    let lowerCasedFirst = []
+    _.forEach(value.split(/\s/g), v => lowerCasedFirst.push(_.toLower(v)))
+    return lowerCasedFirst.length > 0 ? lowerCasedFirst.join(' ') : value
+  }
+}
 
-const parseValue = value => {
-  console.log('Parsing string values');
-  // handle options
-  const err = null;
-  const parsedValue = value;
-  return [err, parsedValue];
-};
+const parseValue = (value, stringify = true) => {
+  let err = null
+  const parsedValue = isString(value)
+    ? value
+    : stringify ? JSON.stringify(value) : undefined
+
+  if (!parsedValue) err = true
+  return [err, parsedValue]
+}
 
 const handler = (
-  options = { min: null, max: null, normalize: null, default: null },
+  options = {
+    min: null,
+    max: null,
+    normalize: null,
+    stringify: true,
+    hideOnFail: false,
+    default: null
+  }
 ) => {
-  const parser = () => {};
+  const parser = () => {}
   parser.parse = value => {
-    let parsedVal = options.default;
-    const [err, result] = parseValue(value);
+    let parsedVal = options.default
 
-    if (!err) parsedVal = result;
+    /**
+     * TODO: Validation min, max here
+     */
 
-    if (!err && options.normalize && NORMALIZER[options.normalize])
-      parsedVal = NORMALIZER[options.normalize](parsedVal);
+    const [err, result] = parseValue(value, options.stringify)
 
-    if (!err && isFunction(options.normalize))
-      parsedVal = options.normalize(parsedVal);
+    if (!err) parsedVal = result
 
-    return parsedVal;
-  };
-  return parser;
-};
+    if (!err && options.normalize && NORMALIZER[options.normalize]) {
+      parsedVal = NORMALIZER[options.normalize](parsedVal)
+    }
 
-module.exports = handler;
+    if (!err && options.normalize && isFunction(options.normalize)) {
+      parsedVal = options.normalize(parsedVal)
+    }
+
+    return [err, parsedVal]
+  }
+
+  parser.isHideOnFail = options.hideOnFail
+
+  return parser
+}
+
+module.exports = handler
