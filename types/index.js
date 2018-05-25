cc8d7dac792dd6a65d9fbaf7dc402147db8e090b
Aditya Kresna Permana <zeandcode@gmail.com>
2018-05-25 15:37:03 +0700

    Exclude string from core function type

--- types/index.js
+++ types/index.js
@@ -1,7 +1,58 @@
 'use strict';
+const { isString, isNumber, isBoolean } = require('./detector');
+const stringType = require('./string');
 
-const string = require('./string');
+const makeHandler = (parserMaker, validate, getOptions) => {
+  const handler = (options = {}) => {
+    options = Object.assign(getOptions(), options);
+
+    const objHandler = () => {};
+
+    objHandler.validate = validate;
+
+    objHandler.parse = parserMaker(options);
+
+    /**
+     * Returning serialized name if set
+     * default is null
+     */
+    objHandler.getSerializeName = () =>
+      (isString(options.serialize.to) || isNumber(options.serialize.to)
+        ? options.serialize.to
+        : null);
+
+    /**
+     * Returning deserialized name if set
+     * default is null
+     */
+    objHandler.getDeserializeName = () =>
+      (isString(options.deserialize.from) || isNumber(options.deserialize.from)
+        ? options.deserialize.from
+        : null);
+
+    /**
+     * Returning status of hide the value on serialization
+     */
+    objHandler.isHideOnSerialization = () =>
+      !(isBoolean(options.serialize.display) ? options.serialize.display : true);
+
+    objHandler.isHideOnDeserialization = () =>
+      !(isBoolean(options.deserialize.display)
+        ? options.deserialize.display
+        : true);
+
+    objHandler.isHideOnFail = () => options.hideOnFail;
+
+    return objHandler;
+  };
+
+  return handler;
+};
 
 module.exports = {
-  string
+  string: makeHandler(
+    stringType.parserMaker,
+    stringType.validate,
+    stringType.getOptions
+  )
 };
--- types/string.js
+++ types/string.js
@@ -1,12 +1,6 @@
 'use strict';
 
-const {
-  isFunction,
-  isString,
-  isArray,
-  isNumber,
-  isBoolean
-} = require('./detector');
+const { isFunction, isString, isArray } = require('./detector');
 const {
   trim,
   toUpper,
@@ -121,50 +115,8 @@ const parserMaker = options => {
   };
 };
 
-const handler = (options = {}) => {
-  options = Object.assign(getOptions(), options);
-
-  const objHandler = () => {};
-
-  objHandler.validate = validate;
-
-  objHandler.parse = parserMaker(options);
-
-  /**
-   * Returning serialized name if set
-   * default is null
-   */
-  objHandler.getSerializeName = () =>
-    (isString(options.serialize.to) || isNumber(options.serialize.to)
-      ? options.serialize.to
-      : null);
-
-  /**
-   * Returning deserialized name if set
-   * default is null
-   */
-  objHandler.getDeserializeName = () =>
-    (isString(options.deserialize.from) || isNumber(options.deserialize.from)
-      ? options.deserialize.from
-      : null);
-
-  /**
-   * Returning status of hide the value on serialization
-   */
-  objHandler.isHideOnSerialization = () =>
-    !(isBoolean(options.serialize.display) ? options.serialize.display : true);
-
-  objHandler.isHideOnDeserialization = () =>
-    !(isBoolean(options.deserialize.display)
-      ? options.deserialize.display
-      : true);
-
-  /**
-   * General methods
-   */
-  objHandler.isHideOnFail = () => options.hideOnFail;
-
-  return objHandler;
+module.exports = {
+  parserMaker,
+  validate,
+  getOptions
 };
-
-module.exports = handler;
