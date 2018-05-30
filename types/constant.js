cb00c3d38cc3bd28e1bb2813296f2a57a385dbc3
Aditya Kresna Permana <zeandcode@gmail.com>
2018-05-30 18:16:59 +0700

    Added constant type

--- types/constant.js
+++ types/constant.js
@@ -1,11 +1,37 @@
 'use strict';
 
-const { isArray } = require('../types/detector');
+const moment = require('moment');
+const { forEach } = require('lodash');
+const {
+  isArray,
+  isString,
+  isNumber,
+  isBoolean,
+  isDate
+} = require('../types/detector');
 const { combineDefaultOptions } = require('./utils');
+const { isBlueprintObject } = require('./utils');
 
 const parserMaker = paramsOrOptions => {
   return (key, value) => {
     if (isArray(paramsOrOptions)) {
+      let valid = false, errorDetails = [];
+
+      forEach(paramsOrOptions, constantValue => {
+        if (!valid) {
+          if (
+            isString(constantValue) ||
+            isNumber(constantValue) ||
+            isBoolean(constantValue)
+          ) {
+            valid = value === constantValue;
+          } else if (isDate(constantValue) || moment.isMoment(constantValue)) {
+          } else if (isBlueprintObject(constantValue)) {
+          }
+        }
+      });
+
+      return [errorDetails, valid];
     }
   };
 };
