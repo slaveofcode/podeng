11d06ab1dc56476851e9013044e0d4d169e1298b
Aditya Kresna Permana <zeandcode@gmail.com>
2018-07-10 10:24:09 +0700

    Added serialize-deserialize test

--- test/conditions_serializer_deserializer.test.js
+++ test/conditions_serializer_deserializer.test.js
@@ -6,19 +6,37 @@ const blueprint = require('../blueprint')
 const types = require('../types')
 
 test('Should be able to use serialize with condition', () => {
-  const Schema = blueprint.object({
-    age: types.conditions(
-      age => age >= 17,
-      types.conditions(
-        age => age >= 30,
-        'You should find your love',
-        'Just having fun right now'
-      ),
-      'Child'
-    ),
+  const Person = blueprint.object({
+    age: types.conditions({
+      evaluates: age => age >= 17,
+      onOk: 'Adult',
+      onFail: 'Child',
+      serialize: {
+        to: 'person_age',
+      },
+    }),
   })
 
-  expect(Schema({ age: 35 })).toEqual({ age: 'You should find your love' })
-  expect(Schema({ age: 20 })).toEqual({ age: 'Just having fun right now' })
-  expect(Schema({ age: 15 })).toEqual({ age: 'Child' })
+  expect(Person({ age: 35 })).toEqual({ age: 'Adult' })
+  expect(Person.serialize({ age: 20 })).toEqual({ person_age: 'Adult' })
+  expect(Person.serialize({ age: 15 })).toEqual({ person_age: 'Child' })
+})
+
+test('Should be able to use deserialize with condition', () => {
+  const Person = blueprint.object({
+    age: types.conditions({
+      evaluates: age => age >= 17,
+      onOk: 'Adult',
+      onFail: 'Child',
+      deserialize: {
+        from: 'the_age',
+      },
+    }),
+  })
+
+  expect(Person({ age: 35 })).toEqual({ age: 'Adult' })
+  expect(Person.serialize({ age: 20 })).toEqual({ age: 'Adult' })
+  expect(Person.serialize({ age: 15 })).toEqual({ age: 'Child' })
+  expect(Person.deserialize({ the_age: 15 })).toEqual({ age: 'Child' })
+  expect(Person.deserialize({ the_age: 20 })).toEqual({ age: 'Adult' })
 })
